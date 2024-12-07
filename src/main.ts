e5712dbcfe2f66aa627187ad2bfd1d73edf9dbab
wangyang1749 <1749748955@qq.com>
2024-12-07 16:41:38 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+import "./styles/tailwind.css";
 import "./styles/main.css";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
 
 export function count(x: number, y: number) {
   return x + y;
